628ccd06c314fb9a4727fe7b8aedc046307c6484
Shiina♪ <44128092+AikoMidori@users.noreply.github.com>
2018-10-15 00:02:15 +0200

    Add files via upload

--- update.bat
+++ update.bat
@@ -2,3 +2,4 @@
 powershell -Command "(New-Object Net.WebClient).DownloadFile('https://aikomidori.github.io/steam-dark-mode/webkit.css', 'webkit.css')"
 powershell -Command "(New-Object Net.WebClient).DownloadFile('https://aikomidori.github.io/steam-dark-mode/README.md', 'README.md')"
 powershell -Command "(New-Object Net.WebClient).DownloadFile('https://aikomidori.github.io/steam-dark-mode/installer.bat', 'installer.bat')"
+powershell -Command "(New-Object Net.WebClient).DownloadFile('https://aikomidori.github.io/steam-dark-mode/Changes.txt', 'Changes.txt')"
