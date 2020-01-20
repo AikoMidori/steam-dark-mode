b492c057e7c6fe28e97b4842f920d77788f4509c
Shiina♪ <44128092+AikoMidori@users.noreply.github.com>
2020-01-20 02:43:18 +0100

    Update SteamDarkModeTool.bat

--- installer/SteamDarkModeTool.bat
+++ installer/SteamDarkModeTool.bat
@@ -49,7 +49,7 @@ if not exist webkit.css (echo webkit.css not found, make sure it's in the same d
 echo Checking for Steam directory and current skin...
 for /f "tokens=1,2*" %%E in ('reg query HKEY_CURRENT_USER\Software\Valve\Steam\') do (
     if %%E==SteamPath set SteamPath=%%G
-    if %%E==SkinV4 set SteamSkin=%%G
+    if %%E==SkinV5 set SteamSkin=%%G
 )
 
 if exist "%SteamPath%" (echo Steam directory found! && echo.) else (echo Steam directory not found. && echo Confirm Steam is installed and try running this file as administrator. && pause && goto:eof)
@@ -64,7 +64,7 @@ if exist "%SteamPath%/tmp" (rmdir "%SteamPath%/tmp" && echo Success! && echo.) e
 
 echo Moving Steam Dark Mode skin to Skin directory...
 if [%SteamSkin%]==[] (
-    reg add HKEY_CURRENT_USER\Software\Valve\Steam /v SkinV4 /t REG_SZ /d "SteamDarkMode" /f >nul
+    reg add HKEY_CURRENT_USER\Software\Valve\Steam /v SkinV5 /t REG_SZ /d "SteamDarkMode" /f >nul
     mkdir "%SteamPath%/skins/SteamDarkMode/resource" >nul
     copy /Y webkit.css "%WebkitPath%" >nul
 )
@@ -98,7 +98,7 @@ if exist "%SteamPath%/tmp" (rmdir "%SteamPath%/tmp" && echo Write access granted
 
 echo Downloading Steam Dark Mode skin to Skin directory...
 if [%SteamSkin%]==[] (
-    reg add HKEY_CURRENT_USER\Software\Valve\Steam /v SkinV4 /t REG_SZ /d "SteamDarkMode" /f >nul
+    reg add HKEY_CURRENT_USER\Software\Valve\Steam /v SkinV5 /t REG_SZ /d "SteamDarkMode" /f >nul
     mkdir "%SteamPath%/skins/SteamDarkMode/resource" >nul
     powershell -Command "Try{[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12;(New-Object Net.WebClient).DownloadFile('https://raw.githubusercontent.com/AikoMidori/steam-dark-mode/master/webkit.css', '%WebkitPath%')}Catch{Write-Warning $($error[0]);pause}"
 )
